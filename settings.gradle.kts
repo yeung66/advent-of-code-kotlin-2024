ba29d8fcfd91992045ded61135dd25954e053715
GitHub Action <action@github.com>
2024-12-15 11:45:40 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,4 @@
-pluginManagement {
-    plugins {
-        kotlin("jvm") version "2.1.0"
-    }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
-}
-rootProject.name = "Advent of Code Kotlin Template"
+rootProject.name = "advent-of-code-kotlin-2024"
 
 dependencyResolutionManagement {
     repositories {
